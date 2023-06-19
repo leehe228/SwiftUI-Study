d4696512f97562dfd7c27347a06a0755bb6f4682
leehe228 <hoeunlee228@gmail.com>
2023-06-19 22:24:50 +0900

    Add: 02 BuildingListsAndNavigation.swiftpm

---
+++ 02 BuildingListsAndNavigation.swiftpm/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad Pro (12.9-inch) (2nd Generation)"], 
+                id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
