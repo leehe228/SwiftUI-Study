f92105c0098de6c3cb9fc0f869cbc8885ef93fa8
leehe228 <hoeunlee228@gmail.com>
2023-06-24 17:40:55 +0900

    Add: 05 AnimatingViewsandTransitions

---
+++ 05 AnimatingViewsandTransitions.swiftpm/Helpers/CircleImage.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .resizable()
+            .frame(width: 240, height: 240)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlelock"))
+    }
+}
