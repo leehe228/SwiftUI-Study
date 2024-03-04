885d3ca504586744c597a63efc524949a8960892
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-05 00:21:43 +0900

    study

--- SwiftStudy/2_basic_operators.swift
+++ SwiftStudy/2_basic_operators.swift
@@ -1,1 +1,17 @@
 // 2. Basic Operators
+
+let b = 10
+let a = 5
+a = b
+
+let (x, y) = (1, 2)
+
+// 대입 연산자는 값을 반환하지 않음
+
+/* ----------
+ * 산술 연산자 Arithmetic Operators
+ * ---------- */
+
+ print("Hello, " + "world")
+
+ 
